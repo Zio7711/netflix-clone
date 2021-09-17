5cb88e8f8ce7c20df7405c4947fb277783120360
Zio <zi.ao.tan@outlook.com>
2021-09-17 14:50:03 +0800

    update banner styles

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../axios';
 import requests from '../requests';
+import './Banner.scss';
 
 function Banner() {
   const [movie, setMovie] = useState({});
@@ -17,6 +18,10 @@ function Banner() {
     fetchData();
   }, []);
 
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  };
+
   return (
     <header
       className='banner'
@@ -27,13 +32,15 @@ function Banner() {
       }}
     >
       <div className='banner_contents'>
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className='banner_title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className='banner_buttons'>
           <button className='banner_button'>Play</button>
           <button className='banner_button'>My List</button>
         </div>
 
-        <h1 className='banner_description'>{movie?.overview}</h1>
+        <h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
       </div>
     </header>
   );
