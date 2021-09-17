cc87c140602b432b202993a3f1870036ec0c2060
Zio <zi.ao.tan@outlook.com>
2021-09-17 15:47:49 +0800

    add youtube component

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,4 +1,6 @@
+import movieTrailer from 'movie-trailer';
 import React, { useEffect, useState } from 'react';
+import YouTube from 'react-youtube';
 import axios from '../axios';
 import './Row.scss';
 
@@ -6,6 +8,8 @@ const base_url = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -15,6 +19,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.title || movie?.name)
+        .then((url) => {
+          console.log(movie);
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
   return (
     <div className='row'>
       <h2>{title}</h2>
@@ -22,6 +48,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -30,6 +57,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
