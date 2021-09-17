f54c5d1e0f840293c90a757f6ca0f8a3ad203ec5
Zio <zi.ao.tan@outlook.com>
2021-09-17 15:17:20 +0800

    add nav bar component

---
+++ src/components/Nav.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.scss';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav_black'}`}>
+      <img
+        className='nav_logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png'
+        alt='Netflix Logo'
+      />
+
+      <img
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-yQFL1YOsN3esm6p1jB1HT-Q6qKtxtZqh9LGwMDIgDCy-p54eMf8jdGSN6yZUeySqseA&usqp=CAU'
+        alt='Netflix Logo'
+        className='nav_avatar'
+      />
+    </div>
+  );
+}
+
+export default Nav;
