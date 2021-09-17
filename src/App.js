5ae4ec6c4016c78b3ae00afa40ffc82bfd31f8e9
Zio <zi.ao.tan@outlook.com>
2021-09-17 09:16:48 +0800

    create row component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import Row from './components/Row';
+import requests from './requests';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Row
+        title='NETFLIX ORIGINALS'
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title='NETFLIX Trending Now' fetchUrl={requests.fetchTrending} />
+    </div>
+  );
 }
 
 export default App;
