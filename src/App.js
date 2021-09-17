d50aeb610ab57f466567f11b7d4495bbbdc29236
Zio <zi.ao.tan@outlook.com>
2021-09-17 14:11:13 +0800

    get all movies and build rows, change the originals to large pics

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import requests from './requests';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Row
         title='NETFLIX ORIGINALS'
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
       />
       <Row title='NETFLIX Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
